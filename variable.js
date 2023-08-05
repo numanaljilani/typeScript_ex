9354150e65aed8fa7f3dda2b67ff47e10eaf160c
numanaljilani <numanaljilani001@gmail.com>
2023-08-05 18:14:41 +0530

    this is primitive data type in type scrpt

---
+++ variable.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = "20";
+console.log(a, "hello friend this is typscripts notes");
---
+++ variable.ts
@@ -0,0 +1,4 @@
+const a : number = "20"
+console.log(a , "hello friend this is typscripts notes")
+
+export {}
